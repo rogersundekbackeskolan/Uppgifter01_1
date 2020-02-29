d28f770e83d29de2fdd901b50e8dee65e135aa64
rogersundekbackeskolan <roger.sund@ekbackeskolan.se>
2020-02-29 19:21:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Gör ett program som läser in värden till tre #variabler, summerar dessa samt skriver ut resultatet #till skärmen.
+
+tal1 = float(input("Skriv in ett tal: "))
+tal2 = float(input("Skriv in ett tal till: "))
+tal3 = float(input("Skriv in ett tredje tal: "))
+
+summan = tal1 + tal2 + tal3
+
+print("Summan blir ", summan)
+
+
